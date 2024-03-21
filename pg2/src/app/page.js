a2f22baca227bcece26a8957d89357ce22047a5e
Gavin P <gavinapersad@gmail.com>
2024-03-21 18:12:36 +0000

    added from HMTL of 1.0

--- pg2/src/app/page.js
+++ pg2/src/app/page.js
@@ -3,6 +3,15 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>watagwan</>
+    <>
+      <img src="example.png" alt="Example" />
+      <div className="container">
+        <div className="input-section">
+          <input type="text" id="userAnswer" placeholder="Use the phonetic to guess the word..." />
+          <button id="submitBTN">Submit</button>
+        </div>
+        <div className="phonetics" id="phoneticsOutput"></div>
+      </div>
+    </>
   );
 }
