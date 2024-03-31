b34c4995a8e19e16eb0286c1f9bf3d4d6376fad6
Rhys Postans <rmp71171@icloud.com>
2024-03-31 21:50:13 +0100

    config change

--- pg2/next.config.mjs
+++ pg2/next.config.mjs
@@ -2,9 +2,7 @@ import { StrictMode } from 'react';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/pg2",
     output: "export",  // <=== enables static exports
-    reactStrictMode: true,
 };
 
 export default nextConfig;
